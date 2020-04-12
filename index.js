f187012d154ecbd3d11e878d543112d8c434d33c
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-12 15:26:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ element.remove();
 let newHeader = document.createElement('h1#victory');
 
 document.body.appendChild(newHeader);
+
+for (let i = 0; i < 1; i++) {
+  let newHeader = document.querySelector('h1#victory')
+
+}
