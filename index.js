09605e1cc2893ebe389ebf2d219f7e9439339de5
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-12 15:13:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Write your code here!
-let element = document.getElementById('main')
+let element = document.getElementById('main');
 
 element.remove();
+
+let newHeader = document.querySelector('h1#victory');
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
